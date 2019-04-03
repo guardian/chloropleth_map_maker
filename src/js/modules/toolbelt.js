75a3f99a59be6026ae3f9de688980004594fbfc1
Andy Ball <work@andyball.info>
2019-04-03 13:53:57 +1100

    Allow for zero values in colour allocation

--- src/js/modules/toolbelt.js
+++ src/js/modules/toolbelt.js
@@ -123,6 +123,25 @@ export class Toolbelt {
         return result
     }
 
+    niceNumber(num) {
+                if ( num > 0 ) {
+                if ( num > 1000000000 ) { return ( num / 1000000000 ).toFixed(1) + 'bn' }
+                if ( num > 1000000 ) { return ( num / 1000000 ).toFixed(1) + 'm' }
+                if (num % 1 != 0) { return num.toFixed(2) }
+                else { return num.toLocaleString() }
+            }
+
+            if ( num < 0 ) {
+                var posNum = num * -1;
+                if ( posNum > 1000000000 ) return [ "-" + String(( posNum / 1000000000 ).toFixed(1)) + 'bn'];
+                if ( posNum > 1000000 ) return ["-" + String(( posNum / 1000000 ).toFixed(1)) + 'm'];
+                else { return num.toLocaleString() }
+            }
+
+            return num;
+      
+    }
+
     localStorage() {
         if (typeof localStorage !== 'undefined') {
             try {
